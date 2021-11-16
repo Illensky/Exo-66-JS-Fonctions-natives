7709feed7feefee000ad005691e5208e4cd3a9cc
Illensky <alexis.laroche.02240@gmail.com>
2021-11-16 13:49:14 +0100

    exercice done

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+let pEntier = document.getElementById('entier')
+let pFlottant = document.getElementById('flottant')
+let pNonNumerique = document.getElementById('non-numerique')
+let pRandom = document.getElementById('random')
+let pTrunc = document.getElementById('trunc')
+let pReplace = document.getElementById('replace')
+let operande = 10
+
+pEntier.innerText = parseInt(pEntier.innerText)*operande
+
+pFlottant.innerText = (parseFloat(pFlottant.innerText)*operande)+parseInt(pEntier.innerText)
+
+if (isNaN(pNonNumerique.innerText)){
+    pNonNumerique.innerText = 0;
+}
+
+pRandom.innerText = parseInt(Math.random()*100)
+
+pTrunc.innerText = Math.trunc(pTrunc.innerText)
+
+pReplace.innerText = "John Wick .....  bon O.K .. Alexis Laroche"
 console.log(random());
